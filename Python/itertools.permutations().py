34a11aa19a70b253f19db2a69a3a85489444fb20
Ruiyue Wang <phynew0111@gmail.com>
2022-10-21 22:22:44 -0400

    Update itertools.permutations().py

--- Python/itertools.permutations().py
+++ Python/itertools.permutations().py
@@ -30,7 +30,6 @@
 # Your task is to print all possible permutations of size k of the string in lexicographic sorted order.
 
 # Input Format
-
 # A single line containing the space separated string S and the integer value k.
 
 # Constraints
