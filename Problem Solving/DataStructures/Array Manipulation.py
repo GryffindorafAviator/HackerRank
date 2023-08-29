f9ec03d9213ee453cfb8e8fc101af331bba99547
Ruiyue Wang <phynew0111@gmail.com>
2023-08-28 22:49:26 -0400

    Create Array Manipulation.py

---
+++ Problem Solving/DataStructures/Array Manipulation.py
@@ -0,0 +1,47 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'arrayManipulation' function below.
+#
+# The function is expected to return a LONG_INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER n
+#  2. 2D_INTEGER_ARRAY queries
+#
+
+def arrayManipulation(n, queries):
+    arr = [0] * n
+    glb_max = 0
+    
+    for query in queries:
+        for i in range(query[0] - 1, query[1]):
+            arr[i] += query[2]
+            glb_max = max(arr[i], glb_max)
+    
+    return glb_max
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    first_multiple_input = input().rstrip().split()
+
+    n = int(first_multiple_input[0])
+
+    m = int(first_multiple_input[1])
+
+    queries = []
+
+    for _ in range(m):
+        queries.append(list(map(int, input().rstrip().split())))
+
+    result = arrayManipulation(n, queries)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
