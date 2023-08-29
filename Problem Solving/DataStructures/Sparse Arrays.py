c3b84684c696b541da43c80eb6a47c14db8cd993
Ruiyue Wang <phynew0111@gmail.com>
2023-08-28 22:14:22 -0400

    Create Sparse Arrays.py

---
+++ Problem Solving/DataStructures/Sparse Arrays.py
@@ -0,0 +1,61 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'matchingStrings' function below.
+#
+# The function is expected to return an INTEGER_ARRAY.
+# The function accepts following parameters:
+#  1. STRING_ARRAY stringList
+#  2. STRING_ARRAY queries
+#
+
+def matchingStrings(stringList, queries):
+    dictn = {}
+    ans = []
+    
+    for string in stringList:
+        if string in dictn:
+            cnt = dictn[string]
+            dictn[string] = cnt + 1
+        else:
+            dictn[string] = 1
+    
+    for query in queries:
+        if query in dictn:
+            ans.append(dictn[query])
+        else:
+            ans.append(0)
+    
+    return ans
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    stringList_count = int(input().strip())
+
+    stringList = []
+
+    for _ in range(stringList_count):
+        stringList_item = input()
+        stringList.append(stringList_item)
+
+    queries_count = int(input().strip())
+
+    queries = []
+
+    for _ in range(queries_count):
+        queries_item = input()
+        queries.append(queries_item)
+
+    res = matchingStrings(stringList, queries)
+
+    fptr.write('\n'.join(map(str, res)))
+    fptr.write('\n')
+
+    fptr.close()
