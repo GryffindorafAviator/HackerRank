fe95846710fa9540dc40c6f711d6394365354548
Ruiyue Wang <phynew0111@gmail.com>
2023-09-09 23:57:52 -0400

    Update Tree: Postorder Traversal.py

--- Problem Solving/DataStructures/Tree: Postorder Traversal.py
+++ Problem Solving/DataStructures/Tree: Postorder Traversal.py
@@ -40,6 +40,7 @@ self.left (the left child of the node)
 self.right (the right child of the node)
 self.info (the value of the node)
 """
+# Recursive
 def postOrder(root):
     if root == None:
         return
@@ -47,6 +48,27 @@ def postOrder(root):
     postOrder(root.left)
     postOrder(root.right)
     print(root.info, end=' ')
+
+# Iterative
+def postOrder(root):
+    stack = []
+    stack.append(root)
+    rslt = []
+    
+    while stack != []:
+        cur = stack.pop(-1)
+        rslt.append(cur.info)
+        
+        if cur.left != None:
+            stack.append(cur.left)
+        
+        if cur.right != None:
+            stack.append(cur.right)
+    
+    rslt.reverse()
+    
+    for elem in rslt:
+        print(elem, end=' ')
 
 tree = BinarySearchTree()
 t = int(input())
