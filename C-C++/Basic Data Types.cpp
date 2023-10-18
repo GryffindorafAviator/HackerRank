f1afc13e47a043c9f181c3804564709ef20a879b
Ruiyue Wang <phynew0111@gmail.com>
2023-10-18 10:29:26 -0400

    Create Basic Data Types.cpp

---
+++ C-C++/Basic Data Types.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <cstdio>
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int d;
+    long ld;
+    char c;
+    float f;
+    double lf;
+    
+    cin >> d >> ld >> c >> f >> lf;
+    cout << d << endl;
+    cout << ld << endl;
+    cout << c << endl;
+    cout << fixed << setprecision(3) << f << endl;
+    cout << fixed << setprecision(9) << lf << endl;
+    
+    return 0;
+}
